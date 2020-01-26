776c16b84cfe58e9e137e6b05f275b483b3a2b4c
dimajkezh <dimajkezh@gmail.com>
2020-01-27 01:44:16 +0200

    'first'

---
+++ pages/index.vue
@@ -0,0 +1,80 @@
+<template>
+  <div class="main_wrapper">
+    <div class="header_wrapper">
+      <div class="header">
+        <div class="header_img">
+          <img src="avatar.png" alt="">
+        </div>
+        <div class="header_text">
+          <p class="header_title">Вероника Ростова</p>
+          <p class="header_subtitle">Менеджер по продажам</p>
+          <div class="header_description">Подберу для вас самые лучшие предложения. <br> Мои услуги абсолютны</div>
+        </div>
+      </div>
+    </div>
+    <div class="services">
+      <div class="services_head">Услуг</div>
+      <div class="services_items">
+        <div class="services_item">
+          <div class="item_text">Ручное бронирование</div>
+          <div class="item_count">11</div>
+        </div>
+        <div class="services_item">
+          <div class="item_text">Пакетные туры</div>
+          <div class="item_count">3</div>
+        </div>
+        <div class="services_item">
+          <div class="item_text">Отели</div>
+          <div class="item_count">1</div>
+        </div>
+      </div>
+      <div class="services_total">
+        <p>Всего</p>
+        <p class="total">15</p>
+      </div>
+    </div>
+    <div class="reviews">
+      <div class="reviews_head">
+        <div class="reviews_head_text">
+          <p class="title">Последние отзывы </p>
+          <a href="#">Все отзывы</a>
+        </div>
+        <div class="reviews_head_stats">
+          <div class="stats_item"><img src="like.png" alt=""><span>131</span></div>
+          <div class="stats_item"><img src="comment.png" alt=""><span>14</span></div> 
+        </div>
+      </div>
+      <div class="reviews_items">
+        <div class="reviews_item">
+          <div class="item_head">
+            <p class="name">Самуил</p>
+            <span class="date">13 октября 2011</span>
+          </div>
+          <div class="item_text">Привет, Верунь! ниче себе ты крутая. фотка класс!!!! </div>
+        </div>
+        <div class="reviews_item">
+          <div class="item_head">
+            <p class="name">Лилия Семёновна</p>
+            <span class="date">14 октября 2011</span>
+          </div>
+          <div class="item_text">Вероника, здравствуйте! Есть такой вопрос: Особый вид куниц жизненно стабилизирует кинетический момент, это и есть всемирно известный центр огранки алмазов и торговли бриллиантами?</div>
+        </div>
+        <div class="reviews_item">
+          <div class="item_head">
+            <p class="name">Лилия Семёновна</p>
+            <span class="date">14 октября 2011</span>
+          </div>
+          <div class="item_text">Вероника, здравствуйте! Есть такой вопрос: Особый вид куниц жизненно стабилизирует кинетический момент?</div>
+        </div>
+      </div>
+    </div>
+    <div class="add_review">
+      <textarea rows='4'></textarea>
+      <button>Написать консультанту</button>
+    </div>
+  </div>
+</template>
+
+<style lang="sass">
+  .heading 
+</style>
